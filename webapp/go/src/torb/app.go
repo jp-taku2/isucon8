bb941b08ffe6e671872616b15ad304b2d90f4c54
Yuta Sakata <yusaka39@gmail.com>
2018-09-16 12:54:58 +0900

    reformat codes

--- webapp/go/src/torb/app.go
+++ webapp/go/src/torb/app.go
@@ -878,7 +878,19 @@ func getSalesByEvent(c echo.Context) error {
 }
 
 func getSales(c echo.Context) error {
-	rows, err := db.Query("select r.*, s.rank as sheet_rank, s.num as sheet_num, s.price as sheet_price, e.id as event_id, e.price as event_price from reservations r inner join sheets s on s.id = r.sheet_id inner join events e on e.id = r.event_id order by reserved_at asc for update")
+	rows, err := db.Query(`
+select
+  r.*,
+  s.rank as sheet_rank,
+  s.num as sheet_num,
+  s.price as sheet_price,
+  e.id as event_id,
+  e.price as event_price
+from reservations r
+  inner join sheets s on s.id = r.sheet_id
+  inner join events e on e.id = r.event_id
+order by reserved_at asc for update
+`)
 	if err != nil {
 		return err
 	}
