28e70ed225a363ab900d030eeace15b0d2c76fe9
jp-taku2 <taku.jp0526@gmail.com>
2018-09-16 14:34:02 +0900

    add sakata

--- db/init.sh
+++ db/init.sh
@@ -19,4 +19,4 @@ fi
 mysql -uisucon -h$DB_HOST torb -e 'ALTER TABLE reservations DROP KEY event_id_and_sheet_id_idx'
 gzip -dc "$DB_DIR/isucon8q-initial-dataset.sql.gz" | mysql -uisucon -h$DB_HOST torb
 mysql -uisucon -h$DB_HOST torb -e 'ALTER TABLE reservations ADD KEY event_id_and_sheet_id_idx (event_id, sheet_id)'
-
+mysql -uisucon -h$DB_HOST torb -e 'alter table reservations add index reserved_at_idx(reserved_at);'
